0ec4556483ff84245ef4af53263f184916652879
Akil1928 <akilwatsonch1928@gmail.com>
2026-02-18 18:36:36 -0600

    Clase Player

---
+++ src/main/java/edu/ucr/mvcjson/c4l113/c4h741/labmvcswingjson/model/Player.java
@@ -0,0 +1,16 @@
+package edu.ucr.mvcjson.c4l113.c4h741.labmvcswingjson.model;
+
+
+import java.util.Objects;
+
+public record Player(String name, Sport sport, String position, int shirtNumber) {
+    public Player {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío");
+        }
+        Objects.requireNonNull(sport, "El deporte es obligatorio");
+        if (shirtNumber <= 0) {
+            throw new IllegalArgumentException("El número de camiseta debe ser mayor a 0");
+        }
+    }
+}
