eb58bbb83cef96b6914008f621c89a283279ca5e
Akil1928 <akilwatsonch1928@gmail.com>
2026-02-18 19:50:08 -0600

    module info

--- src/main/java/edu/ucr/mvcjson/c4l113/c4h741/labmvcswingjson/Launcher.java
+++
@@ -1,9 +0,0 @@
-package edu.ucr.mvcjson.c4l113.c4h741.labmvcswingjson;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
